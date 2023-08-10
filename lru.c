47b92798b3407890f0a85b1f06f46700255d15c5
Jebin Shaju <jebinshaju4@gmail.com>
2023-08-10 20:26:02 +0530

    Least recently used

--- lru.c
+++ lru.c
@@ -1,34 +1,97 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+
+int main()
 {
-        int fn, pn, f[50],p[50],t[50],pos,flag1,flag2,page_fault = 0,x=0,i,j,k;
-        
-        printf("Enter the number of frames: ");
-        scanf("%d",&fn);
-        printf("Enter the number of pages: ");
-        scanf("%d",&pn);
-        
-        printf("Enter the page refernce: ");
-        for(i=0;i<pn;i++)
+    int numFrames, numPages, frameArray[50], pageArray[50], referenceCount[50], position, hitFlag, missFlag, pageFaults = 0, i, j, k;
+
+    // Input: Number of frames and pages
+    printf("Enter the number of frames: ");
+    scanf("%d", &numFrames);
+    printf("Enter the number of pages: ");
+    scanf("%d", &numPages);
+
+    // Input: Page references
+    printf("Enter the page references: ");
+    for (i = 0; i < numPages; i++)
+    {
+        scanf("%d", &pageArray[i]);
+    }
+
+    // Initialize frameArray and referenceCount arrays
+    for (j = 0; j < numFrames; j++)
+    {
+        frameArray[j] = -1;         // Initialize frames to -1 (empty)
+        referenceCount[j] = 0;      // Initialize reference counts to 0
+    }
+
+    printf("\nPage Reference \tFrames\n");
+    for (i = 0; i < numPages; i++)
+    {
+        hitFlag = missFlag = 0;
+
+        // Check if the page is already in the frames
+        for (j = 0; j < numFrames; j++)
         {
-                scanf("%d",&p[i]);
+            if (pageArray[i] == frameArray[j])
+            {
+                printf("hit-\t");
+                referenceCount[j] = i + 1;  // Update the reference counter
+                hitFlag = missFlag = 1;    // Set hit and miss flags
+                break;
+            }
         }
-        
-        for(j=0;j<fn;j++){
-                f[j]=-1;
-                
-        }
-        
-        printf("\nPage Reference \tFrames");
-        for(i=0;i<pn;i++){
-                flag1 = flag2 =0;
-                for(j=0;j<fn;j++){
-                        if(p[i]==f[j])
-                        {
-                                printf("hit");
-                                t[j]=i;
-                                flag1 = flag2 =1;
-                        }
+
+        // If not found in frames and empty slot available, insert
+        if (hitFlag == 0)
+        {
+            for (j = 0; j < numFrames; j++)
+            {
+                if (frameArray[j] == -1)
+                {
+                    printf("miss-\t");
+                    frameArray[j] = pageArray[i];
+                    referenceCount[j] = i + 1;  // Update the reference counter
+                    pageFaults++;
+                    missFlag = 1;
+                    break;
                 }
+            }
         }
+
+        // If not found and no empty slot, perform LRU replacement
+        if (missFlag == 0)
+        {
+            printf("miss-\t");
+            position = 0;
+            for (j = 1; j < numFrames; j++)
+            {
+                if (referenceCount[j] < referenceCount[position])
+                {
+                    position = j;  // Find the least recently used frame
+                }
+            }
+            frameArray[position] = pageArray[i];
+            referenceCount[position] = i + 1;  // Update the reference counter
+            pageFaults++;
+        }
+
+        // Display the frame status after each page reference
+        for (j = 0; j < numFrames; j++)
+        {
+            if (frameArray[j] == -1)
+            {
+                printf("-\t");  // Display empty frame
+            }
+            else
+            {
+                printf("%d\t", frameArray[j]);  // Display page in the frame
+            }
+        }
+        printf("\n");
+    }
+
+    // Display total page faults
+    printf("Total Page Faults: %d\n", pageFaults);
+
+    return 0;
 }
